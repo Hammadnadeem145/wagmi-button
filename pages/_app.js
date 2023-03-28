bb84b5a42a29e2b8cd23edfe8177d219e0d4e660
Hammad Nadeem <hammadnadeem145@gmail.com>
2023-03-28 21:14:11 +0500

    error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,52 @@
-import '@/styles/globals.css'
+// import "@/styles/globals.css";
+import { chains ,WagmiConfig, createClient, configureChains } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import Profile from ".";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Network, Alchemy } from '@alch/alchemy-web3';
+
+const settings = {
+    apiKey: "wvI3tQxuq8acbsQRqQP_C0ie1pQDWlCf",
+    network: Network.MumbaiTestet,
+};
+
+const alchemy = new Alchemy(settings);
+
+// Get the latest block
+const latestBlock = alchemy.core.getBlockNumber();
+
+// Get all outbound transfers for a provided address
+alchemy.core
+    .getTokenBalances('0x994b342dd87fc825f66e51ffa3ef71ad818b6893')
+    .then(console.log);
+
+// Get all the NFTs owned by an address
+const nfts = alchemy.nft.getNftsForOwner("0xshah.eth");
+
+// Listen to all new pending transactions
+alchemy.ws.on(
+    { method: "alchemy_pendingTransactions",
+    fromAddress: "0xshah.eth" },
+    (res) => console.log(res)
+);
+
+
+const {  provider, webSocketProvider} = configureChains([Alchemy], [publicProvider()])
+
+const client = createClient({
+  autoConnect: true,
+  getDefaultProvider,
+  provider, webSocketProvider,
+
+});
+
+function App() {
+  return (
+    <WagmiConfig client={client}>
+    
+      <Profile />
+    </WagmiConfig>
+  );
 }
+
+export default App;
